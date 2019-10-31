09503572d23b2934da49cd4259b3662b0a0f791c
messi-wpy <904315105@qq.com>
2019-10-31 23:50:57 +0800

    .

--- app/src/main/java/com/muxi/workbench/ui/MainControl/MainActivity.java
+++ app/src/main/java/com/muxi/workbench/ui/MainControl/MainActivity.java
@@ -30,8 +30,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i(TAG, "onCreate: +application: "+getApplication().getClass().getSimpleName());
         initView();
+
 
     }
 
