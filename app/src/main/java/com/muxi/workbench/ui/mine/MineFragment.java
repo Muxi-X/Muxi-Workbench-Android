49d406ea6e7bf6ed8d3dbba655ba8b668236c630
钟伊凡 <532256565@qq.com>
2020-03-05 21:06:11 +0800

    修改底部导航栏的图标和显示颜色 & 增加应用图标 & 增加初版的个人主页

--- app/src/main/java/com/muxi/workbench/ui/mine/MineFragment.java
+++ app/src/main/java/com/muxi/workbench/ui/mine/MineFragment.java
@@ -1,7 +1,38 @@
 package com.muxi.workbench.ui.mine;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.muxi.workbench.R;
+import com.muxi.workbench.commonUtils.SPUtils;
+import com.muxi.workbench.ui.login.LoginActivity;
+import com.muxi.workbench.ui.login.model.UserWrapper;
 
 public class MineFragment extends Fragment {
 
+    private Button mLogoutBtn;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_mine, container, false);
+
+        mLogoutBtn = view.findViewById(R.id.btn_logout);
+        mLogoutBtn.setOnClickListener(v -> {
+            SPUtils.getInstance(SPUtils.SP_CONFIG).clear();
+            UserWrapper.getInstance().setUser(null);
+            startActivity(new Intent(getContext(), LoginActivity.class));
+        });
+
+        return view;
+    }
 }
