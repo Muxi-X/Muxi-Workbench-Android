e127d1805100a1cb9792b5f531f498c0f01eab11
charming-c <822170415@qq.com>
2021-01-22 20:02:11 +0800

    解决token为空的问题

--- app/src/main/java/com/muxi/workbench/ui/login/model/netcall/LoginUserBean.java
+++ app/src/main/java/com/muxi/workbench/ui/login/model/netcall/LoginUserBean.java
@@ -7,13 +7,13 @@ public class LoginUserBean {
      * oauth_code : 5TGSZN_IM7KZ1I_2J2GQZW
      */
 
-    private String oauthCode;
+    private String oauth_code;
 
-    public String getOauthCode() {
-        return oauthCode;
+    public String getOauth_code() {
+        return oauth_code;
     }
 
-    public void setOauthCode(String oauthCode) {
-        this.oauthCode = oauthCode;
+    public void setOauth_code(String oauth_code) {
+        this.oauth_code = oauth_code;
     }
 }
